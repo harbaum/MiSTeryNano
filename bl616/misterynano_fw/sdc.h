883df078cbe1c2e538edffa0d4526553c693e8e1
Till Harbaum <till@harbaum.org>
2024-01-14 16:26:15 +0100

    Add missing dependency

--- bl616/misterynano_fw/sdc.h
+++ bl616/misterynano_fw/sdc.h
@@ -3,6 +3,13 @@
 
 #include "spi.h"
 
+// up to four image files can be open. E.g. two
+// floppy disks and two ACSI hard drives
+#define MAX_DRIVES  4
+
+// fatfs mounts the card under /sd
+#define CARD_MOUNTPOINT "/sd"
+
 typedef struct {
   char *name;
   unsigned long len;
@@ -16,10 +23,13 @@ typedef struct {
 
 int sdc_init(spi_t *spi);
 int sdc_image_open(int drive, char *name);
-sdc_dir_t *sdc_readdir(char *name, char*);
+sdc_dir_t *sdc_readdir(int drive, char *name, char*);
 int sdc_handle_event(void);
 int sdc_is_ready(void);
 void sdc_lock(void);
 void sdc_unlock(void);
+char *sdc_get_image_name(int drive);
+char *sdc_get_cwd(int drive);
+void sdc_set_default(int drive, const char *name);
 
 #endif // SDC_H
